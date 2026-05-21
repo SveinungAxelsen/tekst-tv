cb5b7cdab8f1d0800abdc5003adeb5fc77694d76
strayt <sstaalnacke@gmail.com>
2026-05-21 18:25:45 +0200

    Initial Commit

---
+++ tekst-tv/tekst_tvApp.swift
@@ -0,0 +1,17 @@
+//
+//  tekst_tvApp.swift
+//  tekst-tv
+//
+//  Created by Sveinung Axelsen on 21/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct tekst_tvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
